b6bc4aefd98d974e528f626cbf631ffc244ee48c
Roman Frołow <rofrol@gmail.com>
2016-03-19 00:26:56 +0100

    jsx

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-import Rx from 'rx';
 import Cycle from '@cycle/core';
-import CycleDOM from '@cycle/dom';
+import { hJSX, makeDOMDriver } from '@cycle/dom';
 
 Cycle.run(
-  (sources) => (
-    { DOM: Rx.Observable.timer(0, 1000).map(
-      i => CycleDOM.h(`div`, `Seconds elapsed test: ${i}`)
-    ) }
+  (drivers) => (
+    { DOM: drivers.DOM.select('input').events('click')
+		.map(ev => ev.target.checked)
+		.startWith(false)
+		.map(toggled =>
+			<div>
+				<input type="checkbox" /> Toggle me
+				<p>{toggled ? 'ON' : 'off'}</p>
+			</div>
+		),
+	}
   ),
-  { DOM: CycleDOM.makeDOMDriver(`#app`) }
+  { DOM: makeDOMDriver('#app') }
 );

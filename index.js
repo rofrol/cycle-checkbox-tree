6338c6bb04b096efba193ec3d82976f3da85439c
Roman Frołow <rofrol@gmail.com>
2016-03-21 00:38:25 +0100

    checkbox tree

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ import { hJSX, makeDOMDriver } from '@cycle/dom';
 Cycle.run(
   (drivers) => (
     { DOM: drivers.DOM.select('input').events('click')
-		.map(ev => ev.target.checked)
+		.map(ev => ({ value: parseInt(ev.target.value, 10), checked: ev.target.checked }))
 		.startWith(false)
 		.map(toggled =>
-			<div>
-				<input type="checkbox" /> Toggle me
-				<p>{toggled ? 'ON' : 'off'}</p>
-			</div>
+			<ul>
+				<li>
+					<input type="checkbox" value="1" />
+					<span>{ toggled.value === 1 && toggled.checked ? 'ON' : 'off' }</span>
+					<ul>
+						<li>
+							<input type="checkbox" value="2" />
+							<span>{ toggled.value === 2 && toggled.checked ? 'ON' : 'off' }</span>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<input type="checkbox" value="3" />
+					<span>{ toggled.value === 3 && toggled.checked ? 'ON' : 'off' }</span>
+				</li>
+			</ul>
 		),
 	}
   ),

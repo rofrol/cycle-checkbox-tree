76aa622b3a413b202647ef49407375c8fb4be36e
Roman Frołow <rofrol@gmail.com>
2016-03-21 02:25:35 +0100

    remember state of checkbox

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 import Cycle from '@cycle/core';
-import { hJSX, makeDOMDriver } from '@cycle/dom';
+import { hJSX, makeDOMDriver, input, ul, li, span } from '@cycle/dom';
+
+const checkboxes = {
+	one: { checked: true },
+	two: { checked: false },
+	three: { checked: false },
+};
 
 Cycle.run(
   (drivers) => (
     { DOM: drivers.DOM.select('input').events('click')
-		.map(ev => ({ value: parseInt(ev.target.value, 10), checked: ev.target.checked }))
-		.startWith(false)
-		.map(toggled =>
-			<ul>
-				<li>
-					<input type="checkbox" value="1" />
-					<span>{ toggled.value === 1 && toggled.checked ? 'ON' : 'off' }</span>
-					<ul>
-						<li>
-							<input type="checkbox" value="2" />
-							<span>{ toggled.value === 2 && toggled.checked ? 'ON' : 'off' }</span>
-						</li>
-					</ul>
-				</li>
-				<li>
-					<input type="checkbox" value="3" />
-					<span>{ toggled.value === 3 && toggled.checked ? 'ON' : 'off' }</span>
-				</li>
-			</ul>
+		.map(ev => Object.assign(checkboxes, { [ev.target.value]: { checked: ev.target.checked } }))
+		.startWith('')
+		.map(() =>
+			ul([
+				li([
+					input(new function(){
+					    this.type = "checkbox";
+						this.value = "one";
+					    checkboxes.one.checked && (this.checked = true);
+					}),
+					span(checkboxes.one.checked ? 'ON' : 'off'),
+					ul([
+						li([
+							input(new function(){
+							    this.type = "checkbox";
+								this.value = "two";
+							    checkboxes.two.checked && (this.checked = true);
+							}),
+							span(checkboxes.two.checked ? 'ON' : 'off'),
+						])
+					]),
+				]),
+				li([
+					input(new function(){
+						this.type = "checkbox";
+						this.value = "three";
+						checkboxes.three.checked && (this.checked = true);
+					}),
+					span(checkboxes.three.checked ? 'ON' : 'off'),
+				]),
+			])
 		),
 	}
   ),
